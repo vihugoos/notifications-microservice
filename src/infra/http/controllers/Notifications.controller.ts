f54eea68ce69e031a231bc3f7db1c642475e72db
Victor Hugo <victorhugoos@live.com>
2022-12-18 10:12:58 -0300

    http module created

---
+++ src/infra/http/controllers/Notifications.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotificationUseCase } from 'src/application/use-cases/SendNotificationUseCase';
+import { CreateNotificationBodyDTO } from '../dtos/CreateNotificationBodyDTO';
+
+@Controller('notifications')
+export class NotificationsController {
+    constructor(private sendNotificationUseCase: SendNotificationUseCase) {}
+
+    @Post()
+    async create(@Body() body: CreateNotificationBodyDTO) {
+        const { recipientId, content, category } = body;
+
+        const { notification } = await this.sendNotificationUseCase.execute({
+            recipientId,
+            content,
+            category,
+        });
+
+        return { notification };
+    }
+}
