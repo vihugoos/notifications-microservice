2bc445d4bab92cd8600a2053bda19d2071aab04f
Victor Hugo <victorhugoos@live.com>
2022-12-19 17:22:43 -0300

    feat(PrismaNotificationsRepository): findById and save methods not yet implemented

--- src/infra/database/prisma/repositories/PrismaNotificationsRepository.ts
+++ src/infra/database/prisma/repositories/PrismaNotificationsRepository.ts
@@ -15,4 +15,12 @@ export class PrismaNotificationsRepository implements INotificationsRepository {
             data: raw,
         });
     }
+
+    async findById(notificationId: string): Promise<Notification | null> {
+        throw new Error('Method not implemented.');
+    }
+
+    async save(notification: Notification): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
 }
