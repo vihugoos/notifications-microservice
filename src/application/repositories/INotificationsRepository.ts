e044f2405bf4287ac7fbaa5536aad79aba51fbdb
Victor Hugo <victorhugoos@live.com>
2022-12-19 17:12:54 -0300

    feat(INotificationsRepository): new findById and save methods

--- src/application/repositories/INotificationsRepository.ts
+++ src/application/repositories/INotificationsRepository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../entities/Notification';
 
 export abstract class INotificationsRepository {
     abstract create(notification: Notification): Promise<void>;
+    abstract findById(notificationId: string): Promise<Notification | null>;
+    abstract save(notification: Notification): Promise<void>;
 }
