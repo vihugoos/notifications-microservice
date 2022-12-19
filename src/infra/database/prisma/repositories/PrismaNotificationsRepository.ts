b26820846e71ed503025a9dd7da2ff631159c8d6
Victor Hugo <victorhugoos@live.com>
2022-12-19 12:28:02 -0300

    feat: PrismaNotificationMapper created

--- src/infra/database/prisma/repositories/PrismaNotificationsRepository.ts
+++ src/infra/database/prisma/repositories/PrismaNotificationsRepository.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { Notification } from '@application/entities/Notification';
 import { INotificationsRepository } from '@application/repositories/INotificationsRepository';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/PrismaNotificationMapper';
 
 @Injectable()
 export class PrismaNotificationsRepository implements INotificationsRepository {
     constructor(private prismaService: PrismaService) {}
 
     async create(notification: Notification): Promise<void> {
+        const raw = PrismaNotificationMapper.toPrisma(notification);
+
         await this.prismaService.notification.create({
-            data: {
-                id: notification.id,
-                recipientId: notification.recipientId,
-                content: notification.content.value,
-                category: notification.category,
-                readAt: notification.readAt,
-                createdAt: notification.createAt,
-            },
+            data: raw,
         });
     }
 }
