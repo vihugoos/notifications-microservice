5dedb0e92de11eb2134e874b421d792e394363bb
Victor Hugo <victorhugoos@live.com>
2022-12-20 12:39:26 -0300

    feat(INotificationsRepository): findManyByRecipientId function

--- src/application/repositories/INotificationsRepository.ts
+++ src/application/repositories/INotificationsRepository.ts
@@ -5,4 +5,7 @@ export abstract class INotificationsRepository {
     abstract findById(notificationId: string): Promise<Notification | null>;
     abstract save(notification: Notification): Promise<void>;
     abstract countManyByRecipientId(recipientId: string): Promise<number>;
+    abstract findManyByRecipientId(
+        recipientId: string,
+    ): Promise<Notification[]>;
 }
