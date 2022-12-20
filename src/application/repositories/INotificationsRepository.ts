f529b017e572205cbfe46a7e1974c5189166491d
Victor Hugo <victorhugoos@live.com>
2022-12-20 20:22:37 -0300

    chore(INotificationsRepository): just re-organized functions

--- src/application/repositories/INotificationsRepository.ts
+++ src/application/repositories/INotificationsRepository.ts
@@ -2,10 +2,10 @@ import { Notification } from '../entities/Notification';
 
 export abstract class INotificationsRepository {
     abstract create(notification: Notification): Promise<void>;
-    abstract findById(notificationId: string): Promise<Notification | null>;
     abstract save(notification: Notification): Promise<void>;
-    abstract countManyByRecipientId(recipientId: string): Promise<number>;
+    abstract findById(notificationId: string): Promise<Notification | null>;
     abstract findManyByRecipientId(
         recipientId: string,
     ): Promise<Notification[]>;
+    abstract countManyByRecipientId(recipientId: string): Promise<number>;
 }
