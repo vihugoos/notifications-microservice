3dbb462fb34d8e07de3016b9c8805d8c40277b98
Victor Hugo <victorhugoos@live.com>
2022-12-21 10:46:15 -0300

    feat(http.module): added all provides

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotificationUseCase } from '@application/use-cases/SendNotificationUseCase';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/Notifications.controller';
+import { CancelNotificationUseCase } from '@application/use-cases/CancelNotificationUseCase';
+import { CountRecipientNotificationsUseCase } from '@application/use-cases/CountRecipientNotificationsUseCase';
+import { GetRecipientNotificationsUseCase } from '@application/use-cases/GetRecipientNotificationsUseCase';
+import { ReadNotificationUseCase } from '@application/use-cases/ReadNotificationUseCase';
+import { UnreadNotificationUseCase } from '@application/use-cases/UnreadNotificationUseCase';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotificationUseCase],
+    providers: [
+        SendNotificationUseCase,
+        CancelNotificationUseCase,
+        CountRecipientNotificationsUseCase,
+        GetRecipientNotificationsUseCase,
+        ReadNotificationUseCase,
+        UnreadNotificationUseCase,
+    ],
 })
 export class HttpModule {}
