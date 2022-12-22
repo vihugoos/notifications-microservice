9c12f4429018c617d4c0672183153c8d78759324
Victor Hugo <victorhugoos@live.com>
2022-12-22 10:34:50 -0300

    feat(app.module): imported MessagingModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
+import { MessagingModule } from '@infra/messaging/messaging.module';
 @Module({
-    imports: [DatabaseModule, HttpModule],
+    imports: [DatabaseModule, HttpModule, MessagingModule],
 })
 export class AppModule {}
